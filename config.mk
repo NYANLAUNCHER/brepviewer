859ee11b6859a8917f91172ccae388a8a6ac0cb7
markiep <purcellmarkie@proton.me>
2025-12-09 20:30:16 -0600

    update

--- config.mk
+++ config.mk
@@ -1,10 +1,12 @@
+# Configure Makefile environment
 ifndef build
 $(error Must provide variable: $$(build))
 endif
 ifndef pname
 $(error Must provide variable: $$(pname))
 endif
+# Top-most directory of project; project root
 export TOP := $(CURDIR)
 export RESDEST := $(build)/share/$(pname)
-# Default flags
+# default flags
 export CXXFLAGS = -g -ggdb -DDEBUG
